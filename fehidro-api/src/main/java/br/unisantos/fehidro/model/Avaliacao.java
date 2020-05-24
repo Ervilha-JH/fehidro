8f890424d8388acf0f305c5f57ae89f6559c960b
ArthurRosendo <62563942+ArthurRosendo@users.noreply.github.com>
2020-05-24 07:32:47 -0300

    Revertido equivoco quanto à classe Avalição

--- fehidro-api/src/main/java/br/unisantos/fehidro/model/Avaliacao.java
+++ fehidro-api/src/main/java/br/unisantos/fehidro/model/Avaliacao.java
@@ -1,25 +1,38 @@
 package br.unisantos.fehidro.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-
-//Essa classe é o grupo de criterios
-//Se está procurando a nota dada pelo avaliador vá para a classe "Nota"
+//Classe que mantem a nota dada por um avaliador
 @Table(name = "tb_avaliacao")
 @Entity
-public class Avaliacao  extends AbstractEntity {
+public class Avaliacao extends AbstractEntity {
 	private static final long serialVersionUID = 1L;
 	
-	@OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "criterio_id")
-	List<CriterioAvaliacao> criterios = new ArrayList<>();
+	@ManyToOne
+    @JoinColumn(name = "nota_id")
+    private Pontuacao nota;
 	
-
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario avaliador;
+    
+    @ManyToOne
+    @JoinColumn(name = "subcriterio_id")
+    private SubcriterioAvaliacao subcriterio;
+    
+    @ManyToOne
+    @JoinColumn(name = "criterio_id")
+    private CriterioAvaliacao criterio;
+    
+    @ManyToOne
+    @JoinColumn(name = "subPDC_id")
+    private SubPDC subpdc;
+    
+    @ManyToOne
+    @JoinColumn(name = "pdc_id")
+    private PDC pdc;
+	
 }
